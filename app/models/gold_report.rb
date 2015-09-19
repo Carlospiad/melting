0e94ece484444e9d38af03d61950b89dac08c92a
carlospiad <carlospiad@gmail.com>
2015-09-18 21:31:48 -0500

    initalize state machines

--- app/models/gold_report.rb
+++ app/models/gold_report.rb
@@ -1,4 +1,24 @@
 class GoldReport < ActiveRecord::Base
   belongs_to :branch
   belongs_to :assistants
+  include AASM
+
+  class GoldReport::State
+    include AASM
+
+    aasm do
+      state :draft, :initial => true
+      state :preapproved
+      state :approved
+
+      event :preapprove do
+         transitions :from => :draft, :to => :preapproved
+      end
+      event :approve do
+         transitions :from => :preapproved, :to => :approved
+      end
+    end
+
+  end
+
 end
