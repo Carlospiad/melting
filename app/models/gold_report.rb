967e44c16399e1bb8fe8a4132a279553ba174746
carlospiad <carlospiad@gmail.com>
2015-09-18 22:26:04 -0500

    fix aasm initializer

--- app/models/gold_report.rb
+++ app/models/gold_report.rb
@@ -1,24 +1,19 @@
 class GoldReport < ActiveRecord::Base
   belongs_to :branch
   belongs_to :assistants
+
   include AASM
+  aasm do
+    state :draft, :initial => true
+    state :preapproved
+    state :approved
 
-  class GoldReport::State
-    include AASM
-
-    aasm do
-      state :draft, :initial => true
-      state :preapproved
-      state :approved
-
-      event :preapprove do
-         transitions :from => :draft, :to => :preapproved
-      end
-      event :approve do
-         transitions :from => :preapproved, :to => :approved
-      end
+    event :preapprove do
+       transitions :from => :draft, :to => :preapproved
     end
-
+    event :approve do
+       transitions :from => :preapproved, :to => :approved
+    end
   end
-
+  
 end
